2d008b8318c107aa8a400cbe62dcc3c0aa90cafd
Shadi Sbaih <shadisbaih20014@gmail.com>
2025-06-28 22:53:18 +0300

    axios Instance done  (recap the file)

--- frontend/blog-app/src/utils/axiosInstance.js
+++ frontend/blog-app/src/utils/axiosInstance.js
@@ -3,33 +3,48 @@ import { BASE_URL } from "./apiPaths";
 
 const axiosInstance = axios.create({
     baseURL: BASE_URL,
-    timeout:60000,
+    timeout: 60000,
     headers: {
         "Content-Type": "application/json",
-        Accept:"application/json",
+        Accept: "application/json",
     },
 });
 
 //request interceptor
 axiosInstance.interceptors.request.use(
-    (config)=>{
+    (config) => {
         const token = localStorage.getItem("token");
-        if(token){
+        if (token) {
             config.headers["Authorization"] = `Bearer ${token}`;
         }
         return config;
     },
-    (error)=>{
+    (error) => {
         return Promise.reject(error);
     }
 )
 
 //response interceptor
 axiosInstance.interceptors.response.use(
-    (response)=>{
+    (response) => {
         return response;
     },
-    (error)=>{
+    (error) => {
+        //handle common errors  globally
+        if (error.response) {
+            if (error.response.status === 401) {
+                //redirect to login page if token is expired
+                window.location.href = "/login";
+            }
+            else if(error.response.status===500){
+                console.error("Server error, please try again later");
+            }
+            else if(error.code==="ECONNABORTED"){
+                console.error("Request timeout, please try again later");
+            }
+        }
         return Promise.reject(error);
     }
 )
+
+export default axiosInstance;
